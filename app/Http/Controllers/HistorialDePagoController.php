6adf6bed8703bd2c2ccce346f2703180afa07635
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-10-19 13:25:32 +0200

    Creo la tabla historial de pago con sus, controladores, modelos y las migraciones

---
+++ app/Http/Controllers/HistorialDePagoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Historial_de_pago;
+use Illuminate\Http\Request;
+
+class HistorialDePagoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Historial_de_pago  $historial_de_pago
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Historial_de_pago $historial_de_pago)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Historial_de_pago  $historial_de_pago
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Historial_de_pago $historial_de_pago)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Historial_de_pago  $historial_de_pago
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Historial_de_pago $historial_de_pago)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Historial_de_pago  $historial_de_pago
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Historial_de_pago $historial_de_pago)
+    {
+        //
+    }
+}
