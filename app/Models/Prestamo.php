5bb04d40a5c0b506a995c2b10840e9ded2758660
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-10-19 15:38:44 +0200

    Agrego la ER. Entre préstamo y historial de pago

--- app/Models/Prestamo.php
+++ app/Models/Prestamo.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Prestamo extends Model
 {
     use HasFactory;
+
+    //Un préstamo pertenece a una cuenta.
+    public function cuenta()
+    {
+        return $this-> belongsTo(Cuenta::class);
+    }
+
+    //Un préstamo tiene un historial de pagos.
+    public function historial ()
+    {
+        return $this-> hasOne(Historial_de_pago::class);
+    }
 }
