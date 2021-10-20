55a2b9367b69f54c0f1c7a6e22401b8a3b2e0fe8
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-10-20 13:29:39 +0200

    Agrego los campos faltantes para poder usar los factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->firstName(),
+            'lastName' => $this->faker->lastName(),
+            'adress' => $this->faker->address(),
+            'phone' => $this->faker->ean8(),
+            'dni' => $this->faker->ean8(),
+            'birthday' => $this->faker->date(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
