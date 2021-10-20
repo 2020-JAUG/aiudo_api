3f21a3a47b53872b8489316e18e84e76d3a2bbae
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-10-20 14:44:25 +0200

    Importo los modelos para poder crear los factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
         \App\Models\User::factory(12)->create();
+        \App\Models\Cuenta::factory(12)->create();
+        \App\Models\Prestamo::factory(12)->create();
+        \App\Models\Historial_de_pago::factory(12)->create();
     }
+
 }
