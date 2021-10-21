6b3690bd2e17cdceb7b336fa34129dfb787f624f
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-10-22 01:53:16 +0200

    Agrego los métodos crear, listar historial de pagos y select para mostrar los valores seleccionados

--- app/Http/Controllers/HistorialDePagoController.php
+++ app/Http/Controllers/HistorialDePagoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Historial_de_pago;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class HistorialDePagoController extends Controller
@@ -14,7 +15,22 @@ class HistorialDePagoController extends Controller
      */
     public function index()
     {
-        //
+        $user = auth()->user();
+
+        if ($user->is_admin == true) {
+            $payments = Historial_de_pago::all();
+
+            return response()->json([
+                'success' => true,
+                'data' => $payments,
+            ], status: 200);
+        } else {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'You do not have access.',
+            ], 400);
+        }
     }
 
     /**
@@ -22,9 +38,46 @@ class HistorialDePagoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $user = auth()->user();
+
+        if ($user->is_admin == true) {
+
+            $this->validate($request, [
+                'fecha_de_pago' => 'required',
+                'metodo_de_pago' => 'required',
+                'status' => 'required',
+                'cuenta_id' => 'required',
+                'prestamo_id' => 'required'
+            ]);
+
+            $payment = Historial_de_pago::create([
+                'fecha_de_pago' => $request->fecha_de_pago,
+                'metodo_de_pago' => $request->metodo_de_pago,
+                'status' => $request->status,
+                'cuenta_id' => $request->cuenta_id,
+                'prestamo_id' => $request->prestamo_id
+            ]);
+
+            if (!$payment) {
+                return response()->json([
+                    'success' => false,
+                    'data' => 'The payment was not processed.'
+                ], status: 400);
+            } else {
+                return response()->json([
+                    'success' => true,
+                    'data' => $payment,
+                ], status: 201);
+            }
+        } else {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'You must be an administrator.',
+            ], status: 400);
+        }
     }
 
     /**
@@ -44,9 +97,32 @@ class HistorialDePagoController extends Controller
      * @param  \App\Models\Historial_de_pago  $historial_de_pago
      * @return \Illuminate\Http\Response
      */
-    public function show(Historial_de_pago $historial_de_pago)
+    public function show()
     {
-        //
+        $user = auth()->user();
+
+        //Función para hacer una doble búsqueda para devolver un dato en conjunto.
+        $userPayments = DB::table('historial_de_pagos')
+            ->join('cuentas', 'historial_de_pagos.cuenta_id', '=', 'cuentas.id')
+            ->select('*')
+            ->where('cuentas.user_id', '=', $user->id)
+            ->select(['fecha_de_pago', 'metodo_de_pago', 'status', 'cuenta_id', 'user_id'])//Filtro los datos que quiero mostrar.
+            ->orderBy('fecha_de_pago', 'desc')//Devuelve los pagos hechos recientemente.
+            ->get();//Con get me devuelve el valor de la consulta.
+
+        if ($user) {
+
+            return response()->json([
+                'success' => true,
+                'data' => $userPayments,
+            ], status: 200);
+        } else {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'You do not have access.',
+            ], status: 400);
+        }
     }
 
     /**
