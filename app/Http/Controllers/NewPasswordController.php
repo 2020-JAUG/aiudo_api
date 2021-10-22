4c3621fc9797621865499dae0d54aae9bccc935e
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-10-22 04:11:43 +0200

    Creo el controlador para resetear la contraseña

---
+++ app/Http/Controllers/NewPasswordController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Validation\Rules\Password as RulesPassword;
+
+class NewPasswordController extends Controller
+{
+    //Esta función se encarga de validar la dirección de correo.
+    public function forgot_password(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        //Método para devolver el slug del estado.
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        if ($status == Password::RESET_LINK_SENT) {
+            return [
+                'status' => __($status)
+            ];
+        }
+
+        throw ValidationException::withMessages([
+            'email' => [trans($status)],
+        ]);
+    }
+
+
+    public function reset(Request $request)
+    {
+        //Aquí se validan los atributos.
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => ['required', 'confirmed', RulesPassword::defaults()],
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($request->password),
+                    'remember_token' => Str::random(60),
+                ])->save();
+
+                $user->tokens()->delete(); //Eliminamos los tokens antiguos.
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        if ($status == Password::PASSWORD_RESET) {
+            return response([
+                'message' => 'Password reset successfully'
+            ]);
+        }
+
+        return response([
+            'message' => __($status)
+        ], 500);
+    }
+}
