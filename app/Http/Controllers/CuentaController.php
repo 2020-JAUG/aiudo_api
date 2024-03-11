aa23dfb56c888946b7a92abb6a1261d535f47ccf
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-03-11 22:18:44 +0100

    Update create method

--- app/Http/Controllers/CuentaController.php
+++ app/Http/Controllers/CuentaController.php
@@ -3,82 +3,82 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cuenta;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Exception;
 
 class CuentaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $user = auth()->user();
-
-        if ($user->is_admin == true) {
-            $accounts = Cuenta::all();
-
-            return response()->json([
-                'success' => true,
-                'data' => $accounts,
-            ], status: 200);
-        } else {
-
+        try {
+            if ($user->is_admin === true) {
+                $accounts = Cuenta::all();
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'You do not have access.',
+                ], 400);
+            }
+        } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
-                'message' => 'You do not have access.',
+                'message' => 'This action cannot be performed.'
             ], 400);
         }
+
+        return response()->json([
+            'success' => true,
+            'data' => $accounts,
+        ], 200);
     }
 
+
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse|string
      */
     public function create(Request $request)
     {
         $user = auth()->user();
-
-        if ($user->is_admin == true) {
-
-            $this->validate($request, [
-                'numero_de_cuenta' => 'required|min:15',
-                'tipo' => 'required',
-                'user_id' => 'required'
-            ]);
-
-            $account = Cuenta::create([
-                'numero_de_cuenta' => $request->numero_de_cuenta,
-                'tipo' => $request->tipo,
-                'user_id' => $request->user_id
-            ]);
-
-            if (!$account) {
-                return response()->json([
-                    'success' => false,
-                    'data' => 'This action cannot be performed.'
-                ], status: 400);
+        DB::beginTransaction();
+        try {
+            $account = null;
+            if (!$user->is_admin === true) {
+                return ('You do not have access.');
             } else {
-                return response()->json([
-                    'success' => true,
-                    'data' => $account,
-                ], status: 201);
-            }
-        } else {
 
-            return response()->json([
-                'success' => false,
-                'message' => 'You must be an administrator.',
-            ], status: 400);
+                $this->validate($request, [
+                    'numero_de_cuenta' => 'required|min:15',
+                    'tipo' => 'required',
+                    'user_id' => 'required'
+                ]);
+
+                $account = Cuenta::create([
+                    'numero_de_cuenta' => $request->numero_de_cuenta,
+                    'tipo' => $request->tipo,
+                    'user_id' => $request->user_id
+                ]);
+            }
+
+            DB::commit();
+        } catch (Exception $ex) {
+            DB::rollback();
+            report($ex);
         }
+        return response()->json([
+            'success' => true,
+            'data' => $account,
+        ], 201);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -86,41 +86,30 @@ class CuentaController extends Controller
         //
     }
 
+
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Cuenta  $cuenta
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
+     * @throws Exception
      */
-    public function show(Cuenta $cuenta)
+    public function show()
     {
         $user = auth()->user();
-
-        if ($user) {
-
-            $account = Cuenta::select(['tipo', 'numero_de_cuenta', 'user_id', 'created_at'])
-                ->get();
-            $account = Cuenta::where('user_id', $user->id)->get();
-
-            if ($user && $account->isEmpty()) {
-                return response()->json([
-                    'success' => false,
-                    'message' => "You don't have an account yet",
-                ], status: 400);
-            } else {
-                return response()->json([
-                    'success' => true,
-                    'message' => "These are your accounts.",
-                    'data' => $account,
-                ], status: 200);
-            }
+        try {
+            $accounts = Cuenta::where('user_id', $user->id)->get();
+        } catch (Exception $ex) {
+            throw new $ex;
         }
+        return response()->json([
+            'success' => true,
+            'message' => "These are your accounts.",
+            'data' => $accounts,
+        ], 200);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Cuenta  $cuenta
+     * @param \App\Models\Cuenta $cuenta
      * @return \Illuminate\Http\Response
      */
     public function edit(Cuenta $cuenta)
@@ -131,8 +120,8 @@ class CuentaController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Cuenta  $cuenta
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Cuenta $cuenta
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Cuenta $cuenta)
@@ -143,7 +132,7 @@ class CuentaController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Cuenta  $cuenta
+     * @param \App\Models\Cuenta $cuenta
      * @return \Illuminate\Http\Response
      */
     public function destroy(Cuenta $cuenta)
